ffec8bb4feea5bd5ad88af347cc68e2b46605944
Aligndzcyln <ceylanali80@gmail.com>
2021-09-07 17:40:53 +0300

    Tested emoji list rendering

--- emoji-search-test/src/App.test.js
+++ emoji-search-test/src/App.test.js
@@ -1,8 +1,25 @@
 import React from "react";
+import { render, screen } from "@testing-library/react";
 import ReactDOM from "react-dom";
 import App from "./App";
+import EmojiResults from "./EmojiResults"
 
-it("renders without crashing", () => {
-  const div = document.createElement("div");
-  ReactDOM.render(<App />, div);
-});
+describe("emoji search test", () => {
+  beforeEach(() => {
+    render(<App></App>);
+  })
+
+  test("renders without crashing", () => {
+    const div = document.createElement("div");
+    ReactDOM.render(<App />, div);
+  });
+
+  test("renders emoji list", () => {
+
+    const items = screen.getAllByText(/Click to copy emoji/i);
+    expect(items.length).toEqual(20);
+  });
+})
+
+
+
